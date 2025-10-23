57dfec6ecf96039636b8f4cd5f5bc77775ea0778
precieux0 <jmakumbusela@gmail.com>
2025-10-24 00:25:01 +0100

    Fix Vercel runtime error

--- api/vless.js
+++ api/vless.js
@@ -1,5 +1,5 @@
-export default function handler(req, res) {
-  res.status(200).json({
+module.exports = (req, res) => {
+  res.json({
     message: "🚀 Serveur VLESS Actif!",
     config: "vless://ef7cccd5-f0af-4ec0-b5e8-991907458dca@ton-domaine.vercel.app:443?security=tls&encryption=none&type=ws&path=%2Fproxy#Mon-Serveur-Vercel"
   });
